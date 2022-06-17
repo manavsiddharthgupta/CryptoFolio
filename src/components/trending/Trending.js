8122e49205dab5183c892e3c51977cce80c46dba
Manav Gupta <2006468@kiit.ac.in>
2022-06-17 13:12:31 +0530

    input field responsive

--- src/components/trending/Trending.js
+++ src/components/trending/Trending.js
@@ -45,7 +45,7 @@ const Trending = (props) => {
   }, [fetchTrendingCoins]);
   //
   return (
-    <div className="mt-4 shadow-[0_4px_12px_rgba(0,0,0,0.1)] w-[95%] mx-auto rounded-md p-6">
+    <div className="mt-4 shadow-[0_4px_12px_rgba(0,0,0,0.1)] w-[95% max-w-[1200px] mx-auto rounded-md p-6">
       <div className="flex items-center mb-4">
         <FontAwesomeIcon className="text-green-700" icon={faArrowTrendUp} />
         <p className="ml-4 font-bold">Trending</p>
