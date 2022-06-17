6541852e5041e0ce7c5ba906e550dd82bb6c1bd4
Manav Gupta <2006468@kiit.ac.in>
2022-06-17 21:05:58 +0530

    reading null data fixed

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = (props) => {
   return (
     <nav className="shadow-[0_3px_8px_rgba(0,0,0,0.24)] h-16 px-8 flex items-center justify-between">
       <div className="flex items-center">
-        <img className="w-10 h-10" src={Diamond}/>
+        <img className="w-10 h-10" alt="logo" src={Diamond}/>
         <h1 className="text-lg font-bold ml-4">CryptoFolio</h1>
       </div>
       <div>
